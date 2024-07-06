af7f2d9a5e4b3f8110d90d77e584075487d83025
chobo <isleofsky76@gmail.com>
2024-07-06 16:06:01 +0900

    page24.js

--- page24.js
+++ page24.js
@@ -18,7 +18,7 @@ document.getElementById('getFortuneButton').addEventListener('click', async () =
     stopButton.style.display = 'none';
 
     try {
-        const response = await fetch('http://localhost:3000/get-fortune', {
+        const response = await fetch('https://port-0-englishwitheasyword-backend-1272llwoib16o.sel5.cloudtype.app/get-fortune', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
