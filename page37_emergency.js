d0d158b3352b63e234acfb1d42e28214cf6ac6da
chobo <isleofsky76@gmail.com>
2024-07-12 14:08:25 +0900

    Update page37_emergency.js

--- page37_emergency.js
+++ page37_emergency.js
@@ -141,7 +141,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     async function generateSentences(topic) {
         try {
-            const response = await fetch('http://localhost:3000/generate-sentences-routines', {
+            const response = await fetch('https://port-0-englishwitheasyword-backend-1272llwoib16o.sel5.cloudtype.app/generate-sentences-routines', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ topic: topic })
