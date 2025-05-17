82d27855c335b9ee22428381a66937d96752ba39
chobo <isleofsky76@gmail.com>
2025-05-17 22:52:33 +0900

    Update page30_guestbook.js

--- page30_guestbook.js
+++ page30_guestbook.js
@@ -146,7 +146,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const password = prompt('Enter password to edit this post:');
         if (password) {
             try {
-                const response = await fetch('https://port-0-englishwitheasyword-backend-1272llwoib16o.sel5.cloudtype.app/viewpost`, {
+                 const response = await fetch(`https://port-0-englishwitheasyword-backend-1272llwoib16o.sel5.cloudtype.app/viewpost`, {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'
@@ -154,6 +154,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     body: JSON.stringify({ id, password })
                 });
 
+            
                 if (response.ok) {
                     const data = await response.json();
                     const entry = data.entry;
